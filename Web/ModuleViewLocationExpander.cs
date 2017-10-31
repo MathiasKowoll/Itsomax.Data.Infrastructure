cae9243fea39388dda6a44b9936afbe4b801855d
Mathias Kowoll <mathias.kowoll@itsomax.com>
2017-10-31 13:50:40 -0300

    Dynamic Views Loading

--- Web/ModuleViewLocationExpander.cs
+++ Web/ModuleViewLocationExpander.cs
@@ -1,37 +1,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Itsomax.Data.Infrastructure.Web
 {
     public class ModuleViewLocationExpander : IViewLocationExpander
     {
-        private const string _moduleKey = "module";
+        private const string MODULE_KEY = "module";
 
         public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
         {
-            if (context.Values.ContainsKey(_moduleKey))
+            context.Values.TryGetValue(MODULE_KEY, out string module);
+            if (!string.IsNullOrWhiteSpace(module))
             {
-                var module = context.Values[_moduleKey];
-                if (!string.IsNullOrWhiteSpace(module))
+                var moduleViewLocations = new string[]
                 {
-                    var moduleViewLocations = new string[]
-                    {
-                    "/Modules/Itsomax.Module." + module + "/Views/{1}/{0}.cshtml",
-                    "/Modules/Itsomax.Module." + module + "/Views/Shared/{0}.cshtml"
-                    };
-
-                    viewLocations = moduleViewLocations.Concat(viewLocations);
-                }
+                    $"/Modules/{module}/Views/{{1}}/{{0}}.cshtml",
+                    $"/Modules/{module}/Views/Shared/{{0}}.cshtml"
+                };
             }
-            return viewLocations;
+
+                /*
+                if (context.Values.ContainsKey(_moduleKey))
+                {
+
+                    if (!string.IsNullOrWhiteSpace(module))
+                    {
+                        var moduleViewLocations = new string[]
+                        {
+                        "/Modules/Itsomax.Module." + module + "/Views/{1}/{0}.cshtml",
+                        "/Modules/Itsomax.Module." + module + "/Views/Shared/{0}.cshtml"
+                        };
+
+                        viewLocations = moduleViewLocations.Concat(viewLocations);
+                    }
+                }
+                */
+                return viewLocations;
         }
 
         public void PopulateValues(ViewLocationExpanderContext context)
         {
-            var controller = context.ActionContext.ActionDescriptor.DisplayName;
-            var moduleName = controller.Split('.')[2];
-            context.Values[_moduleKey] = moduleName;
+            var controllerName = context.ActionContext.ActionDescriptor.DisplayName;
+            var moduleName = controllerName.Split('(', ')')[1];
+            context.Values[MODULE_KEY] = moduleName;
         }
     }
 }
